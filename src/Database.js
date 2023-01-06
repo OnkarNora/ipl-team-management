4336ae5b2e26c2dc835ec4b3335a3b4104c086db
Onkar Nora <onora155@gmail.com>
2023-01-06 11:13:27 +0530

    change on environment variables

--- src/Database.js
+++ src/Database.js
@@ -11,12 +11,12 @@ import {
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyB3a-WIJpXuqxGWRf3eh3GoOTH3dv-0JVE',
-	authDomain: 'team-management-8df94.firebaseapp.com',
-	projectId: 'team-management-8df94',
-	storageBucket: 'team-management-8df94.appspot.com',
-	messagingSenderId: '658793668937',
-	appId: '1:658793668937:web:b08c8b968676a9cb79fa98'
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID
 };
   
 const app = initializeApp(firebaseConfig);
